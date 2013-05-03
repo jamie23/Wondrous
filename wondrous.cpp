ddea2bbc4adcad5166364a09aa8700dddeb47ee5
Jamie <jamie_mcd23@hotmail.com>
2013-05-03 15:00:12 +0100

    README.md

---
+++ wondrous.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void recWonder(int x, int count){
+
+	cout << "Current Number: " << x << endl << "count: " << count << endl << endl;
+
+	if(x!=1){
+		count++;
+		if(x%2==0){
+			recWonder(x/2,count++);
+		}else{
+			recWonder((3*x+1),count++);
+		}
+	}else{
+		cout << "It took : " << count << " amount of calculations." << endl;
+		;
+	}
+}
+
+void recWonder(int x){
+	int count = 0;
+
+	if(x!=1){
+		cout << "Current Number: " << x << endl << "count: " << count << endl << endl;
+		count++;
+		if(x%2==0){
+			recWonder(x/2,count++);
+		}else{
+			recWonder((3*x+1),count++);
+		}
+	}else{
+		cout << "It took : " << count << " calculations." << endl;
+	}
+}
+
+int main(){
+	int number = 0;
+	cout << "Enter a number: ";
+	cin >> number; 
+	recWonder(number);
+	return 0;
+}
