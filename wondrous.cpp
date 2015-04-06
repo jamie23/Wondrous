6436ca119c8534a2f1ddae90cb4237c800a57349
jamie23 <jamie_mcd23@hotmail.com>
2015-04-07 00:52:09 +0100

    Tidied indents

--- wondrous.cpp
+++ wondrous.cpp
@@ -6,41 +6,40 @@ using std::endl;
 
 void recWonder(int x, int count){
 
-	cout << "Current Number: " << x << endl << "count: " << count << endl << endl;
+    cout << "Current Number: " << x << endl << "count: " << count << endl << endl;
 
-	if(x!=1){
-		count++;
-		if(x%2==0){
-			recWonder(x/2,count++);
-		}else{
-			recWonder((3*x+1),count++);
-		}
-	}else{
-		cout << "It took : " << count << " amount of calculations." << endl;
-
-	}
+    if(x!=1){
+        count++;
+        if(x%2==0){
+            recWonder(x/2,count++);
+        }else{
+            recWonder((3*x+1),count++);
+        }
+    }else{
+        cout << "It took : " << count << " amount of calculations." << endl;
+    }
 }
 
 void recWonder(int x){
-	int count = 0;
+    int count = 0;
 
-	if(x!=1){
-		cout << "Current Number: " << x << endl << "count: " << count << endl << endl;
-		count++;
-		if(x%2==0){
-			recWonder(x/2,count++);
-		}else{
-			recWonder((3*x+1),count++);
-		}
-	}else{
-		cout << "It took : " << count << " calculations." << endl;
-	}
+    if(x!=1){
+        cout << "Current Number: " << x << endl << "count: " << count << endl << endl;
+        count++;
+        if(x%2==0){
+            recWonder(x/2,count++);
+        }else{
+            recWonder((3*x+1),count++);
+        }
+    }else{
+        cout << "It took : " << count << " calculations." << endl;
+    }
 }
 
 int main(){
-	int number = 0;
-	cout << "Enter a number: ";
-	cin >> number; 
-	recWonder(number); 
-	return 0; 
+    int number = 0;
+    cout << "Enter a number: ";
+    cin >> number; 
+    recWonder(number); 
+    return 0; 
 }
